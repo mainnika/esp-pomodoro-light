a6201cc63e511f5102db8432593d7bfb0ad1539c
Nikita Tokarchuk <nikita@tokarch.uk>
2024-11-01 07:45:41 +0100

    Add new timer events and states, handle timer transitions.
    - Added new timer events for starting, checking, and completing the timer.
    - Implemented states for idle, work, short break, long break.
    - Updated entry actions to log timer start times.
    - Implemented logic to transition between different timer states based on time intervals.

--- main/pomodoro.cpp
+++ main/pomodoro.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <inttypes.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -36,48 +37,186 @@ extern "C"
 }
 
 struct Off; // forward declaration
-struct Toggle : tinyfsm::Event
+struct Idle;
+struct Work;
+struct ShortBreak;
+struct LongBreak;
+
+// Triggered when the timer is ready to start.
+struct TimerReady : tinyfsm::Event
+{
+};
+// Triggered to start the work period.
+struct StartTimer : tinyfsm::Event
+{
+};
+// Triggered to check the timer.
+struct CheckTimer : tinyfsm::Event
+{
+    int64_t time_since_boot;
+};
+// Triggered when the timer finishes counting down.
+struct TimerComplete : tinyfsm::Event
+{
+};
+// Triggered to reset the timer to the initial state.
+struct ResetTimer : tinyfsm::Event
 {
 };
 
-struct Switch : tinyfsm::Fsm<Switch>
+struct Pomodoro : tinyfsm::Fsm<Pomodoro>
 {
-    virtual void react(Toggle const &) {};
+    virtual void react(StartTimer const &) {};
+    virtual void react(TimerComplete const &) {};
+    virtual void react(ResetTimer const &) = 0;
+    virtual void react(TimerReady const &) {};
+    virtual void react(CheckTimer const &) {};
 
-    // alternative: enforce handling of Toggle in all states (pure virtual)
-    // virtual void react(Toggle const &) = 0;
+    virtual void entry(void)
+    {
+        int64_t time_since_boot = esp_timer_get_time();
+        ESP_LOGI(TAG, "Pomodoro timer started at %" PRIu64 " us", time_since_boot);
 
-    virtual void entry(void) {}; /* entry actions in some states */
-    void exit(void) {};          /* no exit actions */
+        Pomodoro::state_entry_time = time_since_boot;
+    }; /* entry actions in some states */
+    void exit(void) {}; /* no exit actions */
 
-    // alternative: enforce entry actions in all states (pure virtual)
-    // virtual void entry(void) = 0;
+protected:
+    static size_t short_breaks;
+    static size_t long_breaks;
+    static int64_t state_entry_time;
+
+    static constexpr int64_t WORK_PERIOD = 1 * 60 * 1000000;    // 1 minute
+    static constexpr int64_t SHORT_BREAK_PERIOD = 10 * 1000000; // 10 seconds
+    static constexpr int64_t LONG_BREAK_PERIOD = 30 * 1000000;  // 30 seconds
 };
+
+size_t Pomodoro::short_breaks = 0;
+size_t Pomodoro::long_breaks = 0;
+int64_t Pomodoro::state_entry_time = 0;
+constexpr int64_t Pomodoro::WORK_PERIOD;
+constexpr int64_t Pomodoro::SHORT_BREAK_PERIOD;
+constexpr int64_t Pomodoro::LONG_BREAK_PERIOD;
 
 // ----------------------------------------------------------------------------
 // 3. State Declarations
 //
-struct On : Switch
+// The off state where the timer is completely stopped.
+struct Off : Pomodoro
 {
-    void entry() override { ESP_LOGI(TAG, "Switch is ON"); };
-    void react(Toggle const &) override { transit<Off>(); };
+    void entry() override
+    {
+        Pomodoro::entry();
+        ESP_LOGI(TAG, "starting timer");
+    };
+    void react(ResetTimer const &) override {};
+
+    void react(TimerReady const &) override { transit<Idle>(); };
 };
 
-struct Off : Switch
+// The initial state where the timer is ready to start.
+struct Idle : Pomodoro
 {
-    void entry() override { ESP_LOGI(TAG, "Switch is OFF"); };
-    void react(Toggle const &) override { transit<On>(); };
+    void entry() override
+    {
+        Pomodoro::entry();
+        ESP_LOGI(TAG, "timer is ready, idle");
+    };
+    void react(ResetTimer const &) override { transit<Idle>(); };
+
+    void react(StartTimer const &) override { transit<Work>(); };
+};
+// The state where the timer is counting down the work period.
+struct Work : Pomodoro
+{
+    void entry() override
+    {
+        Pomodoro::entry();
+        ESP_LOGI(TAG, "timer is counting");
+    };
+    void react(ResetTimer const &) override { transit<Idle>(); };
+
+    void react(CheckTimer const &check_timer_event) override
+    {
+        int64_t time_since_boot = check_timer_event.time_since_boot;
+        if (time_since_boot - Pomodoro::state_entry_time < Pomodoro::WORK_PERIOD)
+        {
+            ESP_LOGI(TAG, "work time left: %" PRIu64 "", Pomodoro::WORK_PERIOD - (time_since_boot - Pomodoro::state_entry_time));
+            return;
+        }
+
+        if (Pomodoro::short_breaks == 4)
+        {
+            transit<LongBreak>();
+        }
+        else
+        {
+            transit<ShortBreak>();
+        }
+    };
+};
+// The state where the timer is counting down a short break period.
+struct ShortBreak : Pomodoro
+{
+    void entry() override
+    {
+        Pomodoro::entry();
+        Pomodoro::short_breaks++;
+        ESP_LOGI(TAG, "short break %d", Pomodoro::short_breaks);
+    };
+    void react(ResetTimer const &) override { transit<Idle>(); };
+
+    void react(CheckTimer const &check_timer_event) override
+    {
+        int64_t time_since_boot = check_timer_event.time_since_boot;
+        if (time_since_boot - Pomodoro::state_entry_time < Pomodoro::SHORT_BREAK_PERIOD)
+        {
+            ESP_LOGI(TAG, "short break time left: %" PRIu64 "", Pomodoro::SHORT_BREAK_PERIOD - (time_since_boot - Pomodoro::state_entry_time));
+            return;
+        }
+
+        transit<Work>();
+    };
+};
+// The state where the timer is counting down a long break period.
+struct LongBreak : Pomodoro
+{
+    void entry() override
+    {
+        Pomodoro::entry();
+        Pomodoro::short_breaks = 0;
+        ESP_LOGI(TAG, "long break");
+    };
+    void react(ResetTimer const &) override { transit<Idle>(); };
+
+    void react(CheckTimer const &check_timer_event) override
+    {
+        int64_t time_since_boot = check_timer_event.time_since_boot;
+        if (time_since_boot - Pomodoro::state_entry_time < Pomodoro::LONG_BREAK_PERIOD)
+        {
+            ESP_LOGI(TAG, "long break time left: %" PRIu64 "", Pomodoro::LONG_BREAK_PERIOD - (time_since_boot - Pomodoro::state_entry_time));
+            return;
+        }
+
+        transit<Work>();
+    };
 };
 
-FSM_INITIAL_STATE(Switch, Off)
+FSM_INITIAL_STATE(Pomodoro, Off)
 
-using fsm_handle = Switch;
+using fsm_handle = Pomodoro;
 
-Toggle toggle;
 esp_timer_handle_t periodic_timer;
+TimerReady timer_ready_event;
+CheckTimer check_timer_event;
+StartTimer start_timer_event;
+ResetTimer reset_timer_event;
 
 esp_err_t start_timer()
 {
+    fsm_handle::dispatch(timer_ready_event);
+    fsm_handle::dispatch(start_timer_event);
+
     esp_timer_create_args_t periodic_timer_args = {};
 
     periodic_timer_args.callback = &periodic_timer_callback;
@@ -86,7 +225,7 @@ esp_err_t start_timer()
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
 
     /* Start the timers */
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000)); // 1s
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000000));
 
     return ESP_OK;
 }
@@ -117,7 +256,8 @@ void app_main(void)
 static void periodic_timer_callback(void *arg)
 {
     int64_t time_since_boot = esp_timer_get_time();
-    ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
+    ESP_LOGI(TAG, "Periodic timer called, time since boot: %" PRIu64 " us", time_since_boot);
 
-    fsm_handle::dispatch(toggle);
+    check_timer_event.time_since_boot = time_since_boot;
+    fsm_handle::dispatch(check_timer_event);
 }
