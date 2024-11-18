85f49f21e381660421c8df421d457ea1928af99b
Nikita Tokarchuk <nikita@tokarch.uk>
2024-11-18 22:44:56 +0100

    Update GPIO pin configuration for lights
    Changed the pin bit mask to include the green light instead of a different pin. This updates the output setup for better functionality.

--- main/pomodoro.cpp
+++ main/pomodoro.cpp
@@ -427,7 +427,7 @@ static esp_err_t gpio_setup()
 
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1 << GPIO_LIGHT_RED) | (1 << GPIO_LIGHT_YELLOW) | (1 << GPIO_NUM_13);
+    io_conf.pin_bit_mask = (1 << GPIO_LIGHT_RED) | (1 << GPIO_LIGHT_YELLOW) | (1 << GPIO_LIGHT_GREEN);
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
     gpio_config(&io_conf);
